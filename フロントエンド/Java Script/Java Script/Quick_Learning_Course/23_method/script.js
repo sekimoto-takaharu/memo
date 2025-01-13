7d412124c9dc91a4a40abdaf91caa3d4e9efb03d
sekimoto-takaharu <taka20000713@yahoo.co.jp>
2025-01-13 16:26:30 +0900

    JSクイックラーニングコース修了

---
+++ フロントエンド/Java Script/Java Script/Quick_Learning_Course/23_method/script.js
@@ -0,0 +1,21 @@
+/**
+ * メソッドとは */
+
+/* 
+メソッドは、オブジェクトの値にある関数
+*/
+
+let object = {
+    name: "Takaharu",
+    age: 24,
+    holiday() {
+        console.log("hello")
+        console.log("earting breakfast")
+        console.log("studing Java Script")
+        console.log("eating dinner")
+        console.log("sleeping")
+    },
+    sex: "male" 
+}
+
+object.holiday()
