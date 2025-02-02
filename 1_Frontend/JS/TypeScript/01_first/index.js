8c559f7f0b9c9e3a5afb95c0fd31dc34996cf42d
sekimoto-takaharu <taka20000713@yahoo.co.jp>
2025-02-02 23:03:25 +0900

    ファイル名変更

---
+++ 1_Frontend/JS/TypeScript/01_first/index.js
@@ -0,0 +1,3 @@
+// npm install -g typescript で TypeScript をインストール
+var hello = "hello";
+console.log(hello);
---
+++ 1_Frontend/JS/TypeScript/01_first/index.ts
@@ -0,0 +1,7 @@
+// ①npm install -g typescript のコマンドで TypeScript をインストール
+
+let hello: string = "hello";
+console.log(hello);
+
+// ②tsc index.ts のコマンドでコンパイル
+// ③node index.js のコマンドで実行
